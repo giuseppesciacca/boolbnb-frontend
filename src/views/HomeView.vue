f318ccc0ad6434ad3756320340d35120ed33be4d
giuseppesciacca <sciaccagi@gmail.com>
2023-07-11 13:32:46 +0200

    change metod for search input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,7 @@ export default {
             apartments: [],
             all_apartments: null,
             text_to_search: '',
+            text_to_convert: '',
             services: null,
             selected_service: [],
             range: 10,
@@ -19,14 +20,13 @@ export default {
             coordinate: {
                 latitude: null,
                 longitude: null,
-            }
+            },
+            city: '',
         }
     },
     methods: {
         tomtom() {
-            const inputAddress = document.getElementById('address');
             const ulElement = document.getElementById('list_address');
-            const liEl = document.createElement('li');
 
             this.suggestions = []; //svuoto i suggestions
             this.arrayAddressComplete = [] //svuoto l'array di indirizzi
@@ -50,7 +50,7 @@ export default {
 
                             element.forEach(element_inside => {
 
-                                console.log(element_inside);
+                                //console.log(element_inside);
 
                                 const addressComplete = element_inside.address.freeformAddress + ' ' + element_inside.position.lat + ' ' + element_inside.position.lon;
 
@@ -75,6 +75,45 @@ export default {
                         console.log(error);
                     });
             };
+        },
+
+        tomtom2() {
+            const url = 'https://api.tomtom.com/search/2/geocode/';
+            const address = this.text_to_convert + '.json'
+            const apiKey = 'vPuUkOEvt9S93r8E98XRbrHJJG1Mz6Tr';
+            const apiKeyAndOption = 'vPuUkOEvt9S93r8E98XRbrHJJG1Mz6Tr&limit=5&countrySet=IT&entityTypeSet=Municipality&extendedPostalCodesFor=Geo';
+            const urlComplete = url + address + apiKeyAndOption;
+
+            this.coordinate = {
+                latitude: Number,
+                longitude: Number,
+            }
+
+            if (this.text_to_convert.length > 2) {
+                axios.get(urlComplete, {
+                    params: {
+                        key: apiKey
+                    }
+                })
+                    .then(response => {
+                        console.log('Stai cercando la città di ' + response.data.results[0].address.municipality);
+
+                        this.city = response.data.results[0].address.municipality;
+
+
+                        console.log(response.data.results[0].position.lat);
+                        console.log(response.data.results[0].position.lon);
+                        this.coordinate = {
+                            latitude: response.data.results[0].position.lat,
+                            longitude: response.data.results[0].position.lon,
+                        }
+
+                        console.log(this.coordinate);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            }
         },
         /**
          * 
@@ -123,16 +162,30 @@ export default {
 <template>
     <div class="container mt-5">
         <div class="text-center my-5">
-            <label for="address" class="form-label">Indirizzo completo (*)</label>
-            <input type="text" name="address" id="address" class="form-control" placeholder="Dove vuoi andare?"
-                v-model="text_to_search" aria-describedby="nameHelper" @keyup="tomtom()">
 
-            <input type="hidden" name="" id="">
+            <p v-if="this.city == ''">
+                <label for="address" class="form-label">Dove vuoi andare?</label>
+            </p>
+            <p v-else>
+                <label for="address" class="form-label">Stai cercando la città di {{ this.city }}?</label>
+            </p>
+
+            <input type="text" name="address" id="address" class="form-control" placeholder="Dove vuoi andare?"
+                v-model="text_to_convert" aria-describedby="nameHelper" @keyup="tomtom2()">
 
             <ul id="list_address" class="list-unstyled">
-                <!-- lista suggeriti -->
             </ul>
         </div>
+
+        <!--         
+        <div class="text-center my-5">
+            <label for="address" class="form-label">Dove vuoi andare?</label>
+            <input type="text" name="address" id="address" class="form-control" placeholder="Dove vuoi andare?"
+                v-model="text_to_search" aria-describedby="nameHelper" @keyup="tomtom()" v-on:keyup.enter="printlatlon()">
+
+            <ul id="list_address" class="list-unstyled">
+            </ul>
+        </div> -->
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4" v-if="apartments.length > 0">
             <div class="col rounded-4 mb-4" v-for="apartment in apartments" :key="apartment.title">
