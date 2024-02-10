25d17ce32bed9e18662f8ac3a7d6a5b8a8c3b6d5
giuseppesciacca <sciaccagi@gmail.com>
2024-02-10 19:23:46 +0100

    ADD call api-post for view apt

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import { reactive } from 'vue'
+import { reactive } from "vue";
 const store = reactive({
-    active:true,
-    server: 'http://127.0.0.1:8000/',
-    admin: 'admin',
-    end_point_apartments: 'api/apartments/',
-    end_point_services: 'api/services',
-    end_point_messages: 'api/contacts',
-})
+  active: true,
+  server: "http://127.0.0.1:8000/",
+  admin: "admin",
+  end_point_apartments: "api/apartments/",
+  end_point_services: "api/services",
+  end_point_messages: "api/contacts",
+  api_post: {
+    end_point_views: "api/views",
+  },
+});
 
-export { store }
+export { store };
